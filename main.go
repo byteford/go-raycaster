a6d4df65f871265d2663af8d44371d78d05062eb
James Sandford <james.sandford@ecs-digital.co.uk>
2021-02-12 10:50:15 +0000

    feat: move cam to ptr

--- main.go
+++ main.go
@@ -67,19 +67,19 @@ func makeSphereEmis(centre vec3.T, radius float64,
 	reflection, transparency float64) sphere {
 	return sphere{centre, radius, radius * radius, surfaceColor, emissionsColor, reflection, transparency}
 }
-func trace(rayorig, raydir vec3.T, spheres []sphere, depth int) vec3.T {
+func trace(rayorig, raydir vec3.T, spheres *[]sphere, depth int) vec3.T {
 	tnear := math.Inf(1)
 	//const Sphere* sphere = NULL;
 	var sph *sphere
-	for i := 0; i < len(spheres); i++ {
-		inter, t0, t1 := spheres[i].intersect(&rayorig, &raydir)
+	for i := 0; i < len(*spheres); i++ {
+		inter, t0, t1 := (*spheres)[i].intersect(&rayorig, &raydir)
 		if inter {
 			if t0 < 0 {
 				t0 = t1
 			}
 			if t0 < tnear {
 				tnear = t0
-				sph = &spheres[i]
+				sph = &(*spheres)[i]
 			}
 		}
 	}
@@ -121,22 +121,22 @@ func trace(rayorig, raydir vec3.T, spheres []sphere, depth int) vec3.T {
 		surfaceColor = vec3.Mul(
 			&temp, &sph.surfaceColor)
 	} else {
-		for i := 0; i < len(spheres); i++ {
-			if spheres[i].emissionsColor[0] > 0 {
+		for i := 0; i < len(*spheres); i++ {
+			if (*spheres)[i].emissionsColor[0] > 0 {
 				transmission := vec3.T{1, 1, 1}
-				lightDirection := vec3.Sub(&spheres[i].centre, &phit)
+				lightDirection := vec3.Sub(&(*spheres)[i].centre, &phit)
 				lightDirection.Normalize()
-				for j := 0; j < len(spheres); j++ {
+				for j := 0; j < len((*spheres)); j++ {
 					if i != j {
 						org := vec3.Add(&phit, mulF(nhit, bias))
-						ints, _, _ := spheres[j].intersect(&org, &lightDirection)
+						ints, _, _ := (*spheres)[j].intersect(&org, &lightDirection)
 						if ints {
 							transmission = vec3.Zero
 							break
 						}
 					}
 				}
-				ste := vec3.Mul(&sph.surfaceColor, &spheres[i].emissionsColor)
+				ste := vec3.Mul(&sph.surfaceColor, &(*spheres)[i].emissionsColor)
 				surfaceColor = vec3.Add(&surfaceColor, mulF(vec3.Mul(&ste, &transmission), math.Max(0.0, float64(vec3.Dot(&nhit, &lightDirection)))))
 			}
 		}
@@ -149,7 +149,6 @@ func saveImg(imgc chan *image.RGBA, iterc chan int) {
 		img, more := <-imgc
 		if more {
 			iteration := <-iterc
-			fmt.Printf("%v:%v", iteration, more)
 			f, err := os.Create(fmt.Sprintf("pics/draw%v.jpeg", iteration))
 			if err != nil {
 				panic(err)
@@ -162,12 +161,18 @@ func saveImg(imgc chan *image.RGBA, iterc chan int) {
 		}
 	}
 }
-
-func render(camra cam, spheres []sphere, iteration int, imgc chan *image.RGBA, iterc chan int) {
+func makeImg(pixel, x, y int, imageg []vec3.T, img *image.RGBA) {
+	img.Set(x, y,
+		color.RGBA{uint8(math.Min(float64(1), float64(imageg[pixel][0])) * 255),
+			uint8(math.Min(float64(1), float64(imageg[pixel][1])) * 255),
+			uint8(math.Min(float64(1), float64(imageg[pixel][2])) * 255), 255})
+}
+func render(camra *cam, spheres *[]sphere, iteration int, imgc chan *image.RGBA, iterc chan int) {
 	defer wg.Done()
 	img := image.NewRGBA(image.Rect(0, 0, camra.width, camra.height))
 	imageg := make([]vec3.T, camra.width*camra.height)
 	pixel := 0
+
 	for y := 0; y < camra.height; y++ {
 		for x := 0; x < camra.width; x++ {
 			xx := (2*((float64(x)+0.5)*camra.invWidth) - 1) * camra.angle * camra.aspectratio
@@ -175,7 +180,7 @@ func render(camra cam, spheres []sphere, iteration int, imgc chan *image.RGBA, i
 			rayDir := vec3.T{float32(xx), float32(yy), -1}
 			rayDir.Normalize()
 			imageg[pixel] = trace(vec3.Zero, rayDir, spheres, 0)
-
+			//makeImg(pixel, x, y, imageg, img)
 			img.Set(x, y,
 				color.RGBA{uint8(math.Min(float64(1), float64(imageg[pixel][0])) * 255),
 					uint8(math.Min(float64(1), float64(imageg[pixel][1])) * 255),
@@ -190,7 +195,7 @@ func render(camra cam, spheres []sphere, iteration int, imgc chan *image.RGBA, i
 
 }
 func start() {
-	interations := 1.0
+	interations := 1000.0
 	imgc := make(chan *image.RGBA)
 	iterc := make(chan int)
 	//	width, height                                int
@@ -207,7 +212,7 @@ func start() {
 		spheres = append(spheres, makeSphere(vec3.T{5.0, -1, -5}, 2, vec3.T{0.9, 0.76, 0.46}, 1, 0))
 		spheres = append(spheres, makeSphere(vec3.T{5.0, 0, -15}, 3, vec3.T{0.65, 0.77, 0.97}, 1, 0))
 		wg.Add(1)
-		go render(camra, spheres, i, imgc, iterc)
+		go render(&camra, &spheres, i, imgc, iterc)
 
 	}
 	wg.Wait()
